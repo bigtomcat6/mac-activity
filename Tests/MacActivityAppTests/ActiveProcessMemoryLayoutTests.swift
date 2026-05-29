3ad9313a70c989626af1ee2b7477dae7b1f43c6d
bigtomcat <bigtomcat@qq.com>
2026-05-29 15:17:32 +1000

    feat: add actives process memory layout

---
+++ Tests/MacActivityAppTests/ActiveProcessMemoryLayoutTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import MacActivityApp
+
+final class ActiveProcessMemoryLayoutTests: XCTestCase {
+    func testRowProgressScalesAgainstLargestVisibleProcess() {
+        XCTAssertEqual(ActiveProcessMemoryLayout.progress(bytes: 50, maxBytes: 100), 0.5)
+        XCTAssertEqual(ActiveProcessMemoryLayout.progress(bytes: 150, maxBytes: 100), 1.0)
+        XCTAssertEqual(ActiveProcessMemoryLayout.progress(bytes: 0, maxBytes: 100), 0.0)
+        XCTAssertEqual(ActiveProcessMemoryLayout.progress(bytes: 10, maxBytes: 0), 0.0)
+    }
+
+    func testCompactLayoutConstantsMatchCleanReleasePage() {
+        XCTAssertEqual(ActiveProcessMemoryLayout.rowHeight, 38)
+        XCTAssertEqual(ActiveProcessMemoryLayout.trailingActionWidth, 72)
+    }
+}
